5c0dcc4809cba9101812ad301ca6a9c475da4944
Nash <141100397+s2an@users.noreply.github.com>
2024-01-15 14:32:39 -0500

    Create initial test and model method redemption_count

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,8 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
+  has_many :invoices
+
+  def redemption_count
+    invoices.count
+  end
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -3,5 +3,26 @@ require 'rails_helper'
 RSpec.describe Coupon, type: :model do
   describe "relationships" do
     it { should belong_to(:merchant) }
+    it { should have_many(:invoices) }
+  end
+
+  describe "method tests" do
+    it "counts the number of times it has shown up on an invoice" do
+      coupon = create(:coupon)
+      invoice = create(:invoice, coupon: coupon)
+
+      expect(coupon.redemption_count).to eq(1)
+
+      invoice2 = create(:invoice, coupon: coupon)
+      expect(coupon.redemption_count).to eq(2)
+
+      invoice3 = create(:invoice, coupon: coupon)
+      invoice4 = create(:invoice, coupon: coupon)
+      invoice5 = create(:invoice, coupon: coupon)
+      expect(coupon.redemption_count).to eq(5)
+      
+      # invoice6 = create(:invoice, coupon: coupon)
     end
+  end
+
 end
