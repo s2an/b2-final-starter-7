f8ebe5ccbb47fa0e0b0298f92b5b35e957f79540
Nash <141100397+s2an@users.noreply.github.com>
2024-01-17 00:55:15 -0500

    Complete update to active record and add invoice association I suspected was needed

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,11 +1,9 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
-  # has_many :invoices
+  has_many :invoices
+  has_many :transactions, through: :invoices
 
-  # def redemptions
-  #   # if a coupon is on an invoice
-  #   # and the invoice has a successful transaction
-  #   # add 1 to redemptions
-  #   # if a coupon has 5 redemtions, do not allow to add to an invoice
-  # end
+  def count_successful_uses
+    self.transactions.where(result: "success").count
+  end
 end
